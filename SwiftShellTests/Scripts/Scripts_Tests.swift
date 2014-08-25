8e306fb8d3a593b4f252dfeadf86988d40db3a47
Kare Morstol <kare@nottoobadsoftware.com>
2014-08-26 00:35:03 +0200

    Added function to run test scripts in a unit test. Currently fails with  "<unknown>:0: error: could not load framework 'Foundation'"

--- SwiftShellTests/Scripts/Scripts_Tests.swift
+++ SwiftShellTests/Scripts/Scripts_Tests.swift
@@ -1,29 +1,59 @@
 //
-//  Scripts_Tests.swift
-//  SwiftShell
+// Scripts_Tests.swift
+// SwiftShell
 //
-//  Created by Kåre Morstøl on 24/08/14.
-//  Copyright (c) 2014 NotTooBad Software. All rights reserved.
+// Created by Kåre Morstøl on 24/08/14.
+// Copyright (c) 2014 NotTooBad Software. All rights reserved.
 //
 
-import  SwiftShell
+import SwiftShell
+//import Foundation
 import XCTest
 
 class Scripts_Tests: XCTestCase {
+	
+	let script_folder = "SwiftShellTests/Scripts/"
+	
+	private func runtestscript ( filename: String) -> String {
+		let task = NSTask()
+		task.arguments = ["-c",  "echo \"line 1nline 2nline 3\" | ./swiftshell.bash " + script_folder + filename]
+		task.launchPath = "/bin/bash"
+		
+		task.standardInput = NSPipe ()// to avoid implicit reading of the script's standardInput
+		// task.standardInput = stream( "line 1\nline 2\nline 3")
+		
+		let output = NSPipe ()
+		task.standardOutput = output
+		let error = NSPipe ()
+		task.standardError = error
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-		println( SwiftShell.run("env").read()    )
+		task.launch()
+		task.waitUntilExit()
+		
+		var errorstring = ""
+		switch task {
+		case task.terminationStatus != 0 :
+			errorstring += ("exit code: \(task.terminationStatus), ")
+		case  task.terminationReason == .UncaughtSignal :
+			errorstring += ("uncaught signal, ")
+		default:
+			1
+		}
+		let standarderror = error.fileHandleForReading.read()
+		if !standarderror.isEmpty {
+			errorstring += standarderror
+		}
+		if !errorstring.isEmpty {
+			XCTFail("Script \(filename) failed because: \(errorstring)")
+		}
+		
+		return output.fileHandleForReading.read()
+		
 	}
-
+	
+	
+	func testExample() {
+		print ( runtestscript("print_linenumbers.swift") )
+	}
+	
 }
